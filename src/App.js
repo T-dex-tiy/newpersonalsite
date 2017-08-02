12fb0dc323a31e95c3463b026245bcf8c67520cc
T-Dex <dex.m.mills@gmail.com>
2017-08-02 16:53:15 -0600

    Create profile pic and CSS for top nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/footerLinks2/socialLinks.js';
 import './styles/App.css';
 import About from './components/about';
 import Projects from './components/projects';
+import ProfilePic from './styles/images/ProfilePic.jpg'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <div className="app-header">
           <div className="Logo"><h1> Dex Mills Designs</h1></div>
+          <div><img className="profile" src={ProfilePic}/></div>
        </div>
         <div className="App-intro">
           <div>
