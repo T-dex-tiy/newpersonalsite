aeba228efed1c4a8af50fd692fc7f48fa1742265
T-Dex <dex.m.mills@gmail.com>
2017-08-04 00:28:34 -0600

    Add links to project images

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import WebProjects from '../styles/images/projects/FirstPage.png'
+import WebProjects1 from '../styles/images/projects/FirstPage.png';
+import WebProjects2 from '../styles/images/projects/chucknorris.png';
+import WebProjects3 from '../styles/images/projects/calculator.png';
+import WebProjects4 from '../styles/images/projects/hangman.png';
 
 
 class Projects extends Component{
@@ -11,12 +14,14 @@ class Projects extends Component{
     return (
       <div>
       <div className="profilelinks">
-        <h1>Projects!</h1>
+        <div><h1 className="projects">Projects</h1></div>
+        <div><p>XXXXXXXXXXXXXXXXX</p></div>
+        <div><h4 className="projects">All projects are hosted on Github</h4></div>
         <div className="projectprofiles">
-          <div className="webProjects one"><img className="portfolio" src={WebProjects}/></div>
-          <div className="webProjects two"><img className="portfolio" src={WebProjects}/></div>
-          <div className="webProjects three"><img className="portfolio" src={WebProjects}/></div>
-          <div className="webProjects three"><img className="portfolio" src={WebProjects}/></div>
+          <div className="webProjects one"><a href ="https://t-dex-tiy.github.io/wk-01-icecream/"><img className="portfolio" src={WebProjects1}/></a></div>
+          <div className="webProjects two"><a href= "https://t-dex-tiy.github.io/API-homework-wk-04/"><img className="portfolio" src={WebProjects2}/></a></div>
+          <div className="webProjects three"><a href="https://t-dex-tiy.github.io/calculator/"><img className="portfolio" src={WebProjects3}/></a></div>
+          <div className="webProjects three"><a href="https://t-dex-tiy.github.io/hangman-wk-02/"><img className="portfolio" src={WebProjects4}/></a></div>
         </div>
       </div>
     </div>
