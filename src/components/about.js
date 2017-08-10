d53ce031957c76a027cfcd9f6fd7ad1b8672a37e
T-Dex <dex.m.mills@gmail.com>
2017-08-09 18:01:06 -0600

    Refresh

--- src/components/about.js
+++ src/components/about.js
@@ -17,7 +17,7 @@ class About extends Component{
               </div>
             </div>
             <div className="Hero2">
-              <h1>A season ending ski injury caused me to lose the ability to ski the way that I have the past thirty years. While I had extensive down time from traveling all over North America, I took the oppurtunity to pursue another passion in creating dynamic, stable websites. I pursued this dream thru hard work and spending all of my free time researching and devolping my skill set. I would like to take these skills futher and continue to teach what I have learned. </h1>
+              <h1>A season ending ski injury caused me to lose the ability to ski the way that I have the past thirty years. While I had extensive down time from traveling all over North America, I took the oppurtunity to pursue another passion in creating dynamic, stable websites. I pursued this dream thru hard work and spending all of my free time researching and devolping my skill set. I would like to take these skills futher and continue to teach what I have learned.  </h1>
             </div>
             <div className="links">
               <Projects/>
