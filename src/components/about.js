5c00160ed6efc7d3b335d2a4bc0b61d4a2a74373
T-Dex <dex.m.mills@gmail.com>
2017-08-11 15:12:32 -0600

    Fix CSS

--- src/components/about.js
+++ src/components/about.js
@@ -13,11 +13,17 @@ class About extends Component{
             </div>
             <div className="bio">
               <div className="bioflex">
-                <h1>Adventure based web design and pursuing the next big idea.</h1>
+                <div className="bioheader">
+                  <h1>Adventure based web design and pursuing the next big idea.</h1>
+                </div>
+                <div className="biocontent">
+                  <p>A season ending ski injury caused me to lose the ability to ski the way that I have the past thirty years. While I had extensive down time from traveling all over North America, I took the oppurtunity to pursue another passion in creating dynamic, stable websites. I pursued this dream thru hard work and spending all of my free time researching and devolping my skill set. I would like to take these skills futher and continue to teach what I have learned.
+                  </p>
+                </div>
               </div>
             </div>
             <div className="Hero2">
-              <h1>A season ending ski injury caused me to lose the ability to ski the way that I have the past thirty years. While I had extensive down time from traveling all over North America, I took the oppurtunity to pursue another passion in creating dynamic, stable websites. I pursued this dream thru hard work and spending all of my free time researching and devolping my skill set. I would like to take these skills futher and continue to teach what I have learned.  </h1>
+              <h1>Frameworks!</h1>
             </div>
             <div className="links">
               <Projects/>
