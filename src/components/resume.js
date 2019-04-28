4e22cf337caf672d7a4fb2363bb26e331cdcbfde
T-Dex <dex.m.mills@gmail.com>
2019-04-27 20:13:20 -0600

    Upadted Resume Modal

--- src/components/resume.js
+++ src/components/resume.js
@@ -21,7 +21,8 @@ class Resume extends Component {
             </Modal.Header>
             <Modal.Body>
             <h1 className="resume">Where I've been</h1>
-              <h4 className="resume">CxAlloy</h4>
+              <h3 className="resume">CxAlloy</h3>
+              <h4 className="resume">Front End Windows Application Web Developer</h4>
               <h5 className="resume date">Salt Lake City || November 2018-April 2019</h5>
               <ul className="resume">
                     <li className="resume">Work with team developing Building commissioning app for windows platform </li>
@@ -32,22 +33,24 @@ class Resume extends Component {
                     <li className="resume">Participate in code reviews of windows application  </li>
               </ul>
               <br></br>
-              <h4 className="resume">University of Utah Coding Bootcamp</h4>
+              <h3 className="resume">University of Utah Coding Bootcamp</h3>
+              <h4 className="resume">Teaching Assistant</h4>
               <h5 className="resume date">Salt Lake City || August 2018-Current</h5>
               <ul className="resume">
                     <li className="resume">Helped students in boot camp learn web development basics for Career Growth</li>
                     <li className="resume">Provided guides and assistance to learning full stack web development curriculum, Focusing on HTML/CSS, Javascript, React, MongoDB, SQL and MySql Structures  </li>
                     <li className="resume">Calmly helped students work through issues with code and fostered an environment of problem-solving</li>
               </ul>
-              <h4 className="resume">Cloudviel Mountain Heli</h4>
+              <br></br>
+              <h3 className="resume">Cloudviel Mountain Heli</h3>
+              <h4 className="resume">Front End Web Developer/ Marketing Assoicate</h4>
               <h5 className="resume date">Salt Lake City || November 2017-Feburary 2019</h5>
               <ul className="resume">
-              <li className="resume">Develop a ReactJS application concurrently with mobile app linked to FireBase Database for creating reservations and displaying members photos in visually engaging manner to improve customers interaction and create reservations on customers schedule in quick time frame</li>
-              <li className="resume">Designed and Developed Electron Application for updating, and adding customers information, along with adding media for continuous engagement of customer</li>
-              <li className="resume">Design email marketing campaigns to engage current members and attract new clientele</li>
-              <li className="resume">Created and edited video, blogs and photo content to engage new and existing customers</li>
-              <li className="resume">Researched and documented best technologies for use of applications for creating stable code</li>
-              
+                     <li className="resume">Develop a ReactJS application concurrently with mobile app linked to FireBase Database for creating reservations and displaying members photos in visually engaging manner to improve customers interaction and create reservations on customers schedule in quick time frame</li>
+                    <li className="resume">Designed and Developed Electron Application for updating, and adding customers information, along with adding media for continuous engagement of customer</li>
+                    <li className="resume">Design email marketing campaigns to engage current members and attract new clientele</li>
+                    <li className="resume">Created and edited video, blogs and photo content to engage new and existing customers</li>
+                    <li className="resume">Researched and documented best technologies for use of applications for creating stable code</li>
               </ul>
 
 
